eaeb73d9101685fa4fc05bc6a364f612f83e00e9
GabrielAlvess2018 <gabrielalves.ifpb@gmail.com>
2018-10-10 03:49:34 -0300

    My Django Girls app, first commit

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import psycopg2
 import psycopg2.extras
-conn = psycopg2.connect('dbname=sisvenda user=postgres password=flasknao host=127.0.0.1')
+conn = psycopg2.connect('dbname=usuario user=postgres password=flasknao host=127.0.0.1')
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 from flask import render_template, request, session
 from app import app
@@ -43,6 +43,8 @@ def cadastro():
 
 @app.route('/caixadeentrada', methods=['GET', 'POST'])
 def caixadeentrada():
+	conn = psycopg2.connect('dbname=usuario user=postgres password=flasknao host=127.0.0.1')
+	cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 	cur.execute("SELECT * FROM Mensagem")
 	lista_mensagens = cur.fetchall()
 	return render_template('caixaentrada.html', caixa=lista_mensagens)
@@ -52,15 +54,18 @@ def escrever():
 	if (request.method == 'POST'):
 		remetente = request.form['remetente']
 		destinatario = request.form['destinatario']
-		mensagem = request.form['mensagem']
-		publi = psycopg2.connect('dbname=public user=postgres password=flasknao host=127.0.0.1')
+		mensagemdestino = request.form['mensagem']
+		publi = psycopg2.connect('dbname=publico user=postgres password=flasknao host=127.0.0.1')
 		t = publi.cursor(cursor_factory=psycopg2.extras.DictCursor)
 		t.execute("SELECT chavpublic FROM Public where email= '%s';"%(destinatario))
 		x = t.fetchall()
 		t.close()
-		keypublic = x[0][0]
+		print (x)
+		'''
 		final = b64decode(keypublic)
-		return RSA.importKey(final)
+		chave_publica = RSA.importKey(final)
+		mensagem = encrypt(mensagemdestino, chave_publica)
+		'''
 	return render_template('escrevercliente.html')
 
 @app.route('/cliente', methods=['GET', 'POST'])
@@ -78,12 +83,12 @@ def cliente():
 		cur.execute("INSERT INTO cliente (nome,senha,email,cpf) VALUES ('%s','%s','%s', %s)"%(nome,senha,email,cpf))
 		conn.commit()
 		cur.close()
-		publi = psycopg2.connect('dbname=public user=postgres password=flasknao host=127.0.0.1')
+		publi = psycopg2.connect('dbname=publico user=postgres password=flasknao host=127.0.0.1')
 		t = publi.cursor(cursor_factory=psycopg2.extras.DictCursor)
 		t.execute("INSERT INTO Public (nome,email,chavpublic) VALUES ('%s','%s','%s')"%(nome,email,chavefinalpublic))
 		publi.commit()
 		t.close()
-		priv = psycopg2.connect('dbname=privat user=postgres password=flasknao host=127.0.0.1')
+		priv = psycopg2.connect('dbname=private user=postgres password=flasknao host=127.0.0.1')
 		e = priv.cursor(cursor_factory=psycopg2.extras.DictCursor)
 		e.execute("INSERT INTO Private (nome,email,chavprivat) VALUES ('%s','%s','%s')"%(nome,email,chavefinalprivat))
 		priv.commit()
@@ -96,7 +101,7 @@ def login():
 	if (request.method == 'POST'):
 		email = request.form['email']
 		senha = request.form['password']
-		conn = psycopg2.connect('dbname=sisvenda user=postgres password=flasknao host=127.0.0.1')
+		conn = psycopg2.connect('dbname=usuario user=postgres password=flasknao host=127.0.0.1')
 		cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 		cur.execute("SELECT * FROM cliente;")
 		x = cur.fetchall()
